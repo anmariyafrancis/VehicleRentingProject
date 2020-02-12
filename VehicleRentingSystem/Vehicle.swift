9f9e293b5d544523fbc6df25b22014b7466d1e54
anmariyafrancis <anmariya1915@gmail.com>
2020-02-12 18:08:41 -0500

    [ANMARIYA] Vehicle class created

---
+++ VehicleRentingSystem/Vehicle.swift
@@ -0,0 +1,21 @@
+//
+//  Vehicle.swift
+//  VehicleRentingSystem
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Vehicle
+{
+    var vehicleId:String?
+    var vehicleDescription:String?
+    var companyName:String?
+    var isSelfDrive:String!
+    var driverName:String?
+    var isInsured:String?
+    var insuranceProviderName:String?
+    var noOfSeat:Int?
+}
